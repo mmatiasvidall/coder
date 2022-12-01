3f74e41bd48396b40913315f43d757ec8fd15937
unknown <matividaldupont@gmail.com>
2022-12-01 15:36:41 -0300

    sesion

--- log-In/public/js/index.js
+++ log-In/public/js/index.js
@@ -1,22 +1,33 @@
 const socket = io.connect();
 
 function render(data) {
-    const html = data
-      .map(
-        (msg) => `<li class="clearfix">
+  const html = data
+    .map(
+      (msg) => `<li class="clearfix">
   <div class="message-data">
     <span class="message-data-time">${msg.nombre}</span><span id="date">${msg.date}</span>
   </div>
   <div class="message my-message">${msg.msj}</div>
   </li>`
-      )
-      .join(" ");
-    document.getElementById("mensajes").innerHTML = html;
-  }
-  
+    )
+    .join(" ");
+  document.getElementById("mensajes").innerHTML = html;
+}
+
 function enviarMensaje(event) {
   const current_datetime = new Date();
-  const formatted_date = current_datetime.getFullYear() + "-" + (current_datetime.getMonth() + 1) + "-" + current_datetime.getDate() + " " + current_datetime.getHours() + ":" + current_datetime.getMinutes() + ":" + current_datetime.getSeconds()
+  const formatted_date =
+    current_datetime.getFullYear() +
+    "-" +
+    (current_datetime.getMonth() + 1) +
+    "-" +
+    current_datetime.getDate() +
+    " " +
+    current_datetime.getHours() +
+    ":" +
+    current_datetime.getMinutes() +
+    ":" +
+    current_datetime.getSeconds();
   const nombre = document.getElementById("nombre").value;
   const msj = document.getElementById("chat_mensaje").value;
   document.getElementById("chat_mensaje").value = "";
@@ -24,8 +35,9 @@ function enviarMensaje(event) {
   return false;
 }
 
-function renderProducto (prod) {
-  const html = prod.map(
+function renderProducto(prod) {
+  const html = prod
+    .map(
       (elem) => `<tr>
       <th scope="col">${elem.nombre}</th>
       <th scope="col">${elem.precio}</th>
@@ -40,14 +52,18 @@ function enviarProducto(prod) {
   const nombreProd = document.getElementById("nombreProd").value;
   const precio = document.getElementById("precio").value;
   const logo = document.getElementById("logo").value;
-  socket.emit("new_producto", { nombre: nombreProd, precio: precio, logo: logo});
+  socket.emit("new_producto", {
+    nombre: nombreProd,
+    precio: precio,
+    logo: logo,
+  });
   return false;
 }
 
-  socket.on("mensajes", (data) => {
-    console.log(data);
-    render(data);
-  });
-  socket.on("productos", (prod) => {
-    renderProducto(prod);
-  });
+socket.on("mensajes", (data) => {
+  console.log(data);
+  render(data);
+});
+socket.on("productos", (prod) => {
+  renderProducto(prod);
+});
